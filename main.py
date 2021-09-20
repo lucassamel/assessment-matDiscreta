539b4ae83d7a1baa71ca1965a2e7b7ae35842e12
Lucas Samel <lucas.samel97@gmail.com>
2021-09-20 17:29:12 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+class Caminhao:
+
+    def __init__(self, placa):
+        self.placa = placa
+        self.conj
+
+    pass
+
+
+class Local:
+
+    def __init__(self, idLocal, nomeLocal):
+        self.id = idLocal
+        self.nome = nomeLocal
+
+    pass
+
+
+class ItemEntrega:
+
+    def __init__(self, idItem, nomeItem):
+        self.idItem = idItem
+        self.nome = nomeItem
+
+    pass
